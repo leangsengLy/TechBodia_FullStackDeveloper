4e3232a6eec4220920ee93c0ff8bc7217d5c5484
leangsengLy <edeepleang@gmail.com>
2025-09-29 22:18:19 +0700

    add new project note

---
+++ LS_Project_Noted/LS_Project_Noted/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using LS_Project_Noted.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LS_Project_Noted.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LS_Project_Noted/LS_Project_Noted/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LS_Project_Noted.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
